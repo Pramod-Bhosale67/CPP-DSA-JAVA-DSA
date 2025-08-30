1974ca1b3676b900d1c38242f52275d9fd6ff0e8
Pramod-Bhosale67 <pramodb6716@gmail.com>
2025-08-30 12:18:20 +0530

    added the fundamentals of CPP notes

---
+++ CPP + DSA/Codes/CPP/Fundamentals of CPP/Hello.cpp
@@ -0,0 +1,60 @@
+
+
+// source .cpp
+
+// header file
+#include <iostream>
+
+// namespace
+using namespace std;
+
+
+int main(){
+
+	cout << "Hello World";
+	
+	return 0;
+}
+
+/*
+
+	1. source code -> .cpp
+
+	2. this source code get converted to expanded source code -> .i
+
+	3. This expanded source code is given to compiler (assemble) -> 
+		converts to machine lang. -> .s or .asm
+
+	4. Linker : it mainly links the inbuilt function / user defined function to it defination
+
+	5. mainly converts to executable
+	comipled file -> HDD
+	Run -> RAM
+
+	Loader -> to bring the file from HDD to RAMP
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
