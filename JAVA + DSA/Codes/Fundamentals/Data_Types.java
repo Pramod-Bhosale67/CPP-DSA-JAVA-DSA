0dc2d7bd5eeb35dad8506d5134c402d9e742d828
Pramod-Bhosale67 <pramodb6716@gmail.com>
2025-09-19 23:25:26 +0530

    added the java data types

---
+++ JAVA + DSA/Codes/Fundamentals/Data_Types.java
@@ -0,0 +1,46 @@
+// class
+public class Main{
+	
+	// main function - where execution start
+	public static void main(String[] args) {
+
+		// Types of Data Types
+
+		// Integer Data Types
+
+		int x = 10;
+		System.out.println("Value of the x: " + x);		
+
+		short y = 100;
+		System.out.println("Value of the y: " + y);	
+
+		byte z = 100;
+		System.out.println("Value of the z: " + z);	
+
+		long a = 65535631;
+		System.out.println("Value of the a: " + a);	
+
+
+		// Boolean
+		boolean isTrue = true;
+		System.out.println("Value of the isTrue: " + isTrue);	
+
+
+		// char
+		char ch = '+';  // should contain single char
+		System.out.println("Value of the ch: " + ch);	
+
+		// floating points
+		float b = 14.23F;
+		System.out.println("Value of the b: " + b);	
+
+
+		double d = 45.90;
+		System.out.println("Value of the d: " + d);	
+
+
+
+
+
+    }
+}
---
+++ JAVA + DSA/Codes/Fundamentals/User_Input.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+// class
+public class Main{
+	
+	// main function - where execution start
+	public static void main(String[] args) {
+
+		// Scanner object to scan the input from the keyboard
+		Scanner sc = new Scanner(System.in);
+
+		// integer data type scanning
+
+		int x;
+		x = sc.nextInt();
+
+		Syste.out.println("enter the integer value");
+		int x = sc.nextInt();
+		System.out.println("Value of the x: " + x);
+
+		// boolean values
+		Syste.out.println("enter the Boolean value");
+		boolean b = sc.nextBoolean();
+		System.out.println("Value of the b: " + b);
+
+		// String
+		System.out.println("enter the String");
+		String s = sc.next();
+		System.out.println("Value of the s: " + s);
+
+		System.out.println("enter the new String");
+		String s1 = sc.nextLine();
+		System.out.println("Value of the s1: " + s1);
+
+		// flaot
+		float f = sc.nextFloat();
+
+		double d = sc.nextDouble();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
